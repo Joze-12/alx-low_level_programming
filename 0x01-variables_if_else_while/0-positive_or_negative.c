0cdf81cc2609cd992abea65bbb832354e7199f4e
Joze-12 <66731935+Joze-12@users.noreply.github.com>
2021-02-04 23:07:28 +0300

    variables if else project

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,3 @@
-/*
- * File: 0-positive_or_negative.c
- * Auth: Yoseph Zemede 
- */
- 
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
@@ -25,7 +20,7 @@ int main(void)
 	else if (n < 0)
 		printf("%d is negative\n", n);
 	else
-		printf("%d is zero\n" n);
+		printf("%d is zero\n", n);
 
 	return (0);
 }
