11d7d7905226db9a130fa9a62aa5af9e40713cdb
Joze-12 <66731935+Joze-12@users.noreply.github.com>
2021-02-10 10:47:24 +0300

    C - Functions, nested loops Project

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,14 +8,14 @@
   *	    0 if the number is zero,
   *	    -1 if the number is less than zero.
   */
-int print_sign(int n)
+int print_sign(int num)
 {
-	if (n > 0)
+	if (num > 0)
 	{
 		_putchar('+');
 		return (1);
 	}
-	else if (n == 0)
+	else if (num == 0)
 	{
 		_putchar('0);
 		return (0);
