ee7b32f2c1d87a52ad71de604850b88cafdea7b7
Joze-12 <66731935+Joze-12@users.noreply.github.com>
2021-02-16 13:35:01 -0500

    C - Pointers and Arrays Project

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,11 +8,17 @@ void puts_half(char *str)
 {
 	int len = 0;
 	int index = 0;
-
+	int n;
+	
 	while (str[index++])
 		len++;
-
-	for (index = len / 2; index < len; index++)
+	
+	if ((len % 2) == 0)
+		n = len / 2;
+	else 
+		n = (len + 1) / 2;
+	
+	for (index = n; index < len; index++)
 		_putchar(str[index]);
 	_putchar('\n');
 }
